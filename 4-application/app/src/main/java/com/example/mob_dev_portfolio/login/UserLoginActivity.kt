f44c627747e1b890234f1d26b652c9c0f43ac687
Amanda Alcantara <alcantaraaa@cardiff.ac.uk>
2024-04-28 21:04:43 +0100

    day and night feature

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/login/UserLoginActivity.kt
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/login/UserLoginActivity.kt
@@ -1,24 +1,32 @@
 package com.example.mob_dev_portfolio.login
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
+import com.example.mob_dev_portfolio.R
 import com.example.mob_dev_portfolio.databinding.ActivityUserLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class UserLoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUserLoginBinding
-
     private lateinit var actionBar: ActionBar
-
     private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var switchMode: SwitchCompat
+    private var nightMode: Boolean = false
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Corrected LayoutInflater usage:
-        binding = ActivityUserLoginBinding.inflate(layoutInflater) // Use layoutInflater, not LayoutInflater
+
+        binding = ActivityUserLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         actionBar = supportActionBar!!
@@ -26,6 +34,28 @@ class UserLoginActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+
+        switchMode = findViewById(R.id.switchMode)
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE)
+        nightMode = sharedPreferences.getBoolean("nightMode", false)
+
+        if (nightMode) {
+            switchMode.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        switchMode.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor = sharedPreferences.edit()
+                editor.putBoolean("nightMode", true)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor = sharedPreferences.edit()
+                editor.putBoolean("nightMode", false)
+            }
+            editor.apply()
+        }
 
         binding.logoutBtn.setOnClickListener {
             firebaseAuth.signOut()
